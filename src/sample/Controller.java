7d9693abac1f70fece94a788fe20412b4957f13a
FirstProject <dalila.herh@gmail.com>
2019-03-20 09:18:29 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class Controller {
+    public Label labela;
+
+    public void ispis(ActionEvent actionEvent) {
+        labela.setText("Ne sam, Samire");
+    }
+}
